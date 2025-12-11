6a44b3154572de265fff5f69f0721c994584909b
ricky1211 <rickyalfian751@gmail.com>
2025-12-11 12:07:20 +0700

    file clod

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Menambahkan base URL relatif untuk compatibility yang lebih baik
-  base: './', 
-  
-  // Konfigurasi Rollup (Untuk memastikan ia menemukan index.html)
-  build: {
-    rollupOptions: {
-      input: {
-        // Karena index.html sekarang di root, kita referensikan langsung
-        main: './index.html', 
-      },
-    },
-  },
 });
